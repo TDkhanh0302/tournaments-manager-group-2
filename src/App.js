ea826137fbc94a66f69ca31ffb0f9864f9f4ef64
khanh <baokhanhtlc1902@gmail.com>
2021-08-27 18:24:01 +0700

    re up project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import TournamentsContextProvider from './contexts/TournamentsContext';
+import AllTournament from './Pages/AllTournament/AllTournament';
+import Home from './Pages/Home/Home';
+import LoginForm from './Pages/Login/LoginForm';
+import SignUpForm from './Pages/Login/SignUpForm';
+import NewTournament from './Pages/NewTournament/NewTournament';
+import TournamentBracketHost from './Pages/TournamentDetails/TournamentBracketHost';
+import TournamentPlayerHost from './Pages/TournamentDetails/TournamentPlayerHost';
+import TournamentSetting from './Pages/TournamentDetails/TournamentSetting';
+import YourTournament from './Pages/YourTournament/YourTournament';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TournamentsContextProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/tournaments" component={AllTournament} />
+          <Route path="/your-tournaments" component={YourTournament} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/sign-up" component={SignUpForm} />
+          <Route path="/add-tournament" component={NewTournament} />
+          <Route path="/tournament/bracket/:tournamentId" component={TournamentBracketHost} />
+          <Route path="/tournament/players/:tournamentId" component={TournamentPlayerHost} />
+          <Route path="/tournament/setting/:tournamentId" component={TournamentSetting} />
+          {/* Tạm thời để tất cả các component host */}
+        </Switch>
+      </Router>
+    </TournamentsContextProvider>
   );
 }
 
